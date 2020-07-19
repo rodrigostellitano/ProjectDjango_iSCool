0007f4e03e952009b2bb8c60feddf9a63e2696fa
Rodrigo Stellitano <rodrigostellitano@gmail.com>
2020-07-19 16:49:35 -0300

    Creating model validations

--- iscool/models.py
+++ iscool/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 from django.db.models import *
 # Create your models here.
@@ -12,25 +13,36 @@ class DisciplineModel(models.Model):
     discipline_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
 
 class StudentModel(models.Model):
 
     student_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=250)
-    age = models.IntegerField()
-    discipline = models.ManyToManyField(DisciplineModel)
+    age = models.IntegerField(validators=[MinValueValidator(3), MaxValueValidator(20)])
+    discipline = models.ManyToManyField(DisciplineModel, blank=True)
 
+    def __str__(self):
+        return self.name
 
 
 
 class AvaliationModel(models.Model):
+    SEMESTER = (
+        ('1','1º Semestre'),
+        ('2','2º Semestre'),
+        ('3','3º Semestre'),
+        
+    )
 
     avaliation_id = models.IntegerField(primary_key=True)
-    grade = models.IntegerField()
-    semester = models.IntegerField()
+    semester = models.CharField(max_length=10,
+                                choices=SEMESTER)
+    note = models.FloatField(validators=[MaxValueValidator(10.0),MinValueValidator(0.0)])
     student_id = models.ForeignKey(StudentModel,null=False,on_delete=CASCADE)
     discipline_id = models.ForeignKey(DisciplineModel,null=False,on_delete=CASCADE)
-
+ 
 
 
 
