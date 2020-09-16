f42249859415a8a414df52b982b8b7b052d62a41
Rodrigo Stellitano <rodrigostellitano@gmail.com>
2020-09-16 00:29:12 -0300

    Change of layout, when I opened today it was giving some color error, I decided to change to a simpler one. I made my own changes to the css.

--- iscool/urls.py
+++ iscool/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.isCoolHomePage.as_view() , name="home"),
-    # path('', views.isCoolStudentListView.as_view() , name="home"),'
+    path('', views.isCoolStudentListView.as_view() , name="student_list"),
     path('student/new/', views.isCoolStudentRegisterView.as_view() , name="student_register"),
     path('student/details/<int:pk>', views.isCoolStudentDetailView.as_view() , name="student_detail"),
     path('student/details/<int:pk>/delete', views.isCoolStudentDeleteView.as_view() , name="student_delete"),
